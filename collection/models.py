100f005d82045c6f9565eedb7d019e46ed74626b
John Spiva <johnspiva@gmail.com>
2017-02-16 11:24:29 -0500

    created post model

--- collection/models.py
+++ collection/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Worksheet(models.Model):
@@ -7,3 +8,8 @@ class Worksheet(models.Model):
     description = models.TextField()
     slug = models.SlugField(unique=True)
     user = models.OneToOneField(User, blank=True, null=True)
+
+class Post(models.Model):
+    author = models.TextField()
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
